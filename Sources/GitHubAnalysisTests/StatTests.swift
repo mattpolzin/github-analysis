716cde37f8e21dae9948899faa53c869e4e19bec
Mathew Polzin <matt.polzin@gmail.com>
2018-10-14 15:39:30 -0700

    Add tests that values of Limited Types produce limited values and the opposite for Limitless Types

--- Sources/GitHubAnalysisTests/StatTests.swift
+++ Sources/GitHubAnalysisTests/StatTests.swift
@@ -9,8 +9,9 @@ import XCTest
 import SwiftCheck
 import GitHubAnalysisCore
 
+// MARK: Aggregation
 class StatTests: XCTestCase {
-	func test_aggregateSumInt() {
+	func test_aggregateSum() {
 		property("sum of ints aggregated is accurate") <- forAll { (ints: [Int]) in
 			let sum = ints.reduce(0, +)
 			let stats1 = ints.map(LimitedStat.init(value:))
@@ -19,9 +20,7 @@ class StatTests: XCTestCase {
 			return aggregateSumAndAvg(stats1).total.value == sum
 				&& aggregateSumAndAvg(stats2).total.value == sum
 		}
-	}
-	
-	func test_aggregateSumDouble() {
+		
 		property("sum of doubles aggregated is accurate") <- forAll { (doubles: [Double]) in
 			let sum = doubles.reduce(0, +)
 			let stats1 = doubles.map(LimitedStat.init(value:))
@@ -32,7 +31,7 @@ class StatTests: XCTestCase {
 		}
 	}
 	
-	func test_aggregateAvgInt() {
+	func test_aggregateAvg() {
 		property("avg of ints aggregated is accurate", arguments: .init(replay: (StdGen.init(677045031, 13744243), 0))) <- forAll { (ints: [Int]) in
 			let average = ints.reduce(0) { $0 + Double($1) / Double(ints.count) }
 			let stats1 = ints.map(LimitedStat.init(value:))
@@ -41,9 +40,7 @@ class StatTests: XCTestCase {
 			return aggregateSumAndAvg(stats1).average.value == average
 				&& aggregateSumAndAvg(stats2).average.value == average
 		}
-	}
-	
-	func test_aggregateAvgDouble() {
+		
 		property("avg of doubles aggregated is accurate") <- forAll { (doubles: [Double]) in
 			let average = doubles.reduce(0) { $0 + $1 / Double(doubles.count) }
 			let stats1 = doubles.map(LimitedStat.init(value:))
@@ -51,6 +48,38 @@ class StatTests: XCTestCase {
 			
 			return aggregateSumAndAvg(stats1).average.value == average
 				&& aggregateSumAndAvg(stats2).average.value == average
+		}
+	}
+}
+
+
+// MARK: Limitlessness
+extension StatTests {
+	func test_LimitedStatValuesAreLimited() {
+		property("Limited Types containing Int produce limited values.") <- forAll { (int: BasicStat<Limited, Int>) in
+			return !int.limitless
+		}
+		
+		property("Limited Types containing Double produce limited values.") <- forAll { (double: BasicStat<Limited, Double>) in
+			return !double.limitless
+		}
+		
+		property("Limited Types containing [Double] produce limited values.") <- forAll { (double: BasicStat<Limited, [Double]>) in
+			return !double.limitless
+		}
+	}
+	
+	func test_LimitlessStatValuesAreLimitless() {
+		property("Limitless Types containing Int produce limitless values.") <- forAll { (int: BasicStat<Limitless, Int>) in
+			return int.limitless
+		}
+		
+		property("Limitless Types containing Double produce limitless values.") <- forAll { (double: BasicStat<Limitless, Double>) in
+			return double.limitless
+		}
+		
+		property("Limitless Types containing [Double] produce limitless values.") <- forAll { (double: BasicStat<Limitless, [Double]>) in
+			return double.limitless
 		}
 	}
 }
